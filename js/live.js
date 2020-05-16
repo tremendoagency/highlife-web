bee82972f85f92de5f894f1c61d4e7bfdc5d1d68
tremendo <47454976+tremendoagency@users.noreply.github.com>
2020-05-16 13:20:53 -0300

    2nd edition

--- js/live.js
+++ js/live.js
@@ -7,13 +7,7 @@
     });
   
     $('.lightmode').on( "click", function(event){
-      var $el = $('body').toggleClass('night');
-      if ($el.hasClass('night')) {
-        $('.chat iframe').attr('src', 'https://www.twitch.tv/embed/highlifebsas/chat?darkpopout');
-      }
-      else{
-        $('.chat iframe').attr('src', 'https://www.twitch.tv/embed/highlifebsas/chat');
-      }
+      nightShift();
     });
 
     $('.navbar-toggler').click(function() {
@@ -24,14 +18,11 @@
     //swiper
   
     var live = new Swiper('#live .swiper-container', {
-      speed: 600,
-      parallax: true,
+      speed: 1000,
       grabCursor: true,
+      parallax: true,
       centeredSlides: true,
       slidesPerView: '1',
-      pagination: {
-        el: '.swiper-pagination',
-      },
       keyboard: {
         enabled: true,
         onlyInViewport: false
@@ -43,7 +34,16 @@
     });
     live.on('slideChangeTransitionEnd', function () {
       var acs = document.querySelectorAll('.swiper-slide-active')[0];
+      innerSlides();
     });
+    function innerSlides(){
+      var navSlide = $('.nav-slide.swiper-slide-active').eq(0);
+      if (navSlide.hasClass('has-inner')){
+        var innerSlide = navSlide.attr('data-inner');
+        console.log(innerSlide);
+        setTimeout(innerSlide.swiper.slideNext(), 2000);
+      }
+    }
   
     $.instagramFeed({
       //'username': 'highlifebsas',
@@ -56,10 +56,28 @@
       'margin': 0,
     });
     $(window).on('load', function () {
-      $('.loader-wrapper').animate({top: '-100%'}, 2000, function() {
-        $('#loader').animate({top: '-100%'}, 2000);
+      $('.loader-wrapper').animate({left: '-100%'}, 2000, function() {
+        $('#loader').animate({left: '-100%'}, 2000);
       });
     });
-  
+    function changeHandler(e) {
+      if ($('body').hasClass('night')) {
+        $('.lightmode').click();
+      }
+    }
+    function nightShift(){
+      var $el = $('body').toggleClass('night');
+      if ($el.hasClass('night')) {
+        $('.chat iframe').attr('src', 'https://www.twitch.tv/embed/highlifebsas/chat?darkpopout');
+      }
+      else{
+        $('.chat iframe').attr('src', 'https://www.twitch.tv/embed/highlifebsas/chat');
+      }
+    }
+   
+    document.addEventListener("fullscreenchange", changeHandler, false);
+    document.addEventListener("webkitfullscreenchange", changeHandler, false);
+    document.addEventListener("mozfullscreenchange", changeHandler, false);
+    
   })(jQuery); // End of use strict
   
