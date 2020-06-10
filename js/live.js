54aefb05ac3df182238cd58128587ddc8326f060
tremendo <47454976+tremendoagency@users.noreply.github.com>
2020-06-10 19:10:45 -0300

    v3

--- js/live.js
+++ js/live.js
@@ -1,36 +1,37 @@
 (function($) {
     "use strict"; // Start of use strict
   
-    $('#liveNav li a, .mobile-navbar a').on( "click", function(event){
+    // Responsive Nav
+
+    $('#navbar-toggler').click(function() {
+      $(this).toggleClass('open');
+      $('.mobile-navbar').toggleClass('show');
+    });
+
+    $('nav li a, .mobile-navbar a').on( "click", function(event){
       event.preventDefault();
       live.slideTo($(this).attr('data-slide'));
       $('.mobile-navbar').removeClass('show');
-      $('.navbar-toggler i').removeClass('fa-times');
-    });
-  
-    $('.lightmode').on( "click", function(event){
-      nightShift();
+      $('#navbar-toggler').removeClass('open');
     });
 
-    $('.navbar-toggler').click(function() {
-      $('.mobile-navbar').toggleClass('show');
-      $('.navbar-toggler i').toggleClass('fa-times');
+    $('.lightmode').on( "click", function(event){
+      nightShift();
     });
   
     //swiper
   
     var live = new Swiper('#live .swiper-container', {
-      effect: 'coverflow',
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows : true,
-      },
-      grabCursor: true,
-      centeredSlides: true,
+      effect: 'fade',
+      speed: 300,
+      spaceBetween: 0,
+      freeMode: true,
       slidesPerView: 'auto',
+      grabCursor: true,
+      navigation: {
+        nextEl: '.move-right',
+        prevEl: '.move-left',
+      },
       keyboard: {
         enabled: true,
         onlyInViewport: false
@@ -56,38 +57,36 @@
     // Video
 
     
-    const domain = 'meet.jit.si';
-    const parentElement = document.querySelector("#video");
-    const meetingName = 'TremendoLiveExample';
-    var options = {
-      roomName: meetingName,
-      width: "100%",
-      height: "100%",
-      parentNode: parentElement,
-      //configOverwrite: {
-      //  prejoinPageEnabled: true,
-      //  requireDisplayName: true,
-      //},
-      interfaceConfigOverwrite: {
-        filmStripOnly: true
-        //DEFAULT_BACKGROUND: '#FFFFFF',
-        //VIDEO_QUALITY_LABEL_DISABLED: true,
-        //LANG_DETECTION: true,
-        //TOOLBAR_BUTTONS: ['tileview'],
-        //SHOW_JITSI_WATERMARK: false,
-        //JITSI_WATERMARK_LINK: 'https://jitsi.org',
-        //SHOW_WATERMARK_FOR_GUESTS: false,
-        //SHOW_BRAND_WATERMARK: false,
-        //BRAND_WATERMARK_LINK: '',
-        //SHOW_CHROME_EXTENSION_BANNER: false,
-      }
-    }
-    const api = new JitsiMeetExternalAPI(domain, options);
+    // const domain = 'meet.jit.si';
+    // const parentElement = document.querySelector("#video");
+    // const meetingName = 'TremendoLiveExample';
+    // var options = {
+    //   roomName: meetingName,
+    //   width: "100%",
+    //   height: "100%",
+    //   parentNode: parentElement,
+    //   //configOverwrite: {
+    //   //  prejoinPageEnabled: true,
+    //   //  requireDisplayName: true,
+    //   //},
+    //   interfaceConfigOverwrite: {
+    //     filmStripOnly: true
+    //     //DEFAULT_BACKGROUND: '#FFFFFF',
+    //     //VIDEO_QUALITY_LABEL_DISABLED: true,
+    //     //LANG_DETECTION: true,
+    //     //TOOLBAR_BUTTONS: ['tileview'],
+    //     //SHOW_JITSI_WATERMARK: false,
+    //     //JITSI_WATERMARK_LINK: 'https://jitsi.org',
+    //     //SHOW_WATERMARK_FOR_GUESTS: false,
+    //     //SHOW_BRAND_WATERMARK: false,
+    //     //BRAND_WATERMARK_LINK: '',
+    //     //SHOW_CHROME_EXTENSION_BANNER: false,
+    //   }
+    // }
+    // const api = new JitsiMeetExternalAPI(domain, options);
 
     $(window).on('load', function () {
-      $('.loader-wrapper').animate({left: '-100%'}, 2000, function() {
-        $('#loader').animate({left: '-100%'}, 2000);
-      });
+      $('#loader').animate({left: '-100%'}, 2000);
     });
     function changeHandler(e) {
       if ($('body').hasClass('night')) {
@@ -96,12 +95,12 @@
     }
     function nightShift(){
       var $el = $('body').toggleClass('night');
-      if ($el.hasClass('night')) {
-        $('.chat iframe').attr('src', 'https://www.twitch.tv/embed/highlifebsas/chat?darkpopout');
-      }
-      else{
-        $('.chat iframe').attr('src', 'https://www.twitch.tv/embed/highlifebsas/chat');
-      }
+      // if ($el.hasClass('night')) {
+      //   $('.chat iframe').attr('src', 'https://www.twitch.tv/embed/highlifebsas/chat?darkpopout');
+      // }
+      // else{
+      //   $('.chat iframe').attr('src', 'https://www.twitch.tv/embed/highlifebsas/chat');
+      // }
     }
    
     document.addEventListener("fullscreenchange", changeHandler, false);
